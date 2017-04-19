3b6179145a1e4db50eded89b58bf135934b082c2
Pauline Gaget <lupiane@hotmail.com>
2017-04-19 12:59:36 +0200

    resto-review app operational

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Gaya",
+    address:      "75007 Paris",
+    phone_number: "01 39 10 10 10",
+    category:     "french"
+  },
+  {
+    name:         "Mamma Primi",
+    address:      "75017 Paris",
+    phone_number: "01 33 45 67 78",
+    category:     "italian"
+  },
+  {
+    name:         "Fam Dang",
+    address:      "Rosenthaler Platz, Berlin",
+    phone_number: "05379 6372",
+    category:     "chinese"
+  },
+  {
+    name:         "Matsuri",
+    address:      "75007 Paris",
+    phone_number: "01 23 45 56 78",
+    category:     "japanese"
+  },
+  {
+    name:         "Léon de Bruxelles",
+    address:      "Brussels",
+    phone_number: "0034 192 3629 965",
+    category:     "belgian"
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
